e99b11cf1a5e57f62eaa247245b51eeb0da18067
Joshua Stoutenburg <jehoshua02@gmail.com>
2015-05-28 13:43:14 -0600

    Apply Changes from KSL Header to DDM Menu
    Applies the changes from rejected ksl-header pull request to ddm-menu. On
    menu open:
    
    + Trigger resize on document
    + Add `margin: 0 100% 0 0` to body
    + Add `position: fixed` to ddm-menu-container__content
    
    We observe that it doesn't fix the menu scrolling in Android 4.0.4. In
    fact, the left menu also doesn't scroll.

--- src/menu.js
+++ src/menu.js
@@ -153,6 +153,19 @@ ddm.menu = (function ($) {
     };
 
 
+    var hack = {
+      do: function () {
+        console.log('hack.do');
+        $(document).trigger('resize');
+        $('body').addClass('ddm-menu-scroll-hack');
+      },
+      undo: function () {
+        console.log('hack.undo');
+        $('body').removeClass('ddm-menu-scroll-hack');
+      }
+    };
+
+
 
     /*= events =*/
     scroll.isolate($element.get(0));
@@ -162,11 +175,13 @@ ddm.menu = (function ($) {
       $element.addClass('ddm-menu--open');
       $element.focus();
       $container.addClass(containerClass);
+      hack.do();
     });
 
     $element.on('close.ddm-menu', function () {
       $container.removeClass(containerClass);
       $element.removeClass('ddm-menu--open');
+      hack.undo();
     });
 
     $element.on('toggle.ddm-menu', function () {
